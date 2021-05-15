4f7aaa133dec31d17b2732a3e6f72d0ba046bf2a
pawel <pawel.nowak.90@gmail.com>
2021-05-15 13:02:50 +0200

    second-config-commit

---
+++ src/main/java/io/github/mat3e/App.java
@@ -0,0 +1,15 @@
+package io.github.mat3e;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!")
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World-komunikat");
+    }
+}
